4591562f859d7d0bf5326dad812e4219227ce2db
I <I@DESKTOP-B1LT3JI>
2026-07-25 17:56:33 +0800

    Depth test

--- LaTaleDoujin/LaTaleDoujin.h
+++ LaTaleDoujin/LaTaleDoujin.h
@@ -40,24 +40,14 @@ protected:
     LRESULT WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 private:
-    ComPtr<ID3D11Texture2D> m_Framebuffer1;
-    ComPtr<ID3D11Texture2D> m_Framebuffer2;
-    ComPtr<ID3D11RenderTargetView> m_Framebuffer1View;
-    ComPtr<ID3D11RenderTargetView> m_Framebuffer2View;
-    ComPtr<ID3D11ShaderResourceView> m_Framebuffer1Tex;
-    ComPtr<ID3D11ShaderResourceView> m_Framebuffer2Tex;
-
     std::unique_ptr<Keyboard> m_Keyboard;
     std::unique_ptr<Mouse> m_Mouse;
 
+    std::unique_ptr<CommonStates> m_CommonStates;
     std::unique_ptr<SpriteBatch> m_SpriteBatch;
-    ComPtr<ID3D11PixelShader> m_GaussianBlur;
-    ComPtr<ID3D11Buffer> m_GaussianParametersH;
-    ComPtr<ID3D11Buffer> m_GaussianParametersV;
     ComPtr<ID3D11ShaderResourceView> m_IrisTexture;
 
     std::unique_ptr<PrimitiveBatch2> m_PrimitiveBatch;
     std::unique_ptr<BasicEffect> m_PrimitiveEffect;
     ComPtr<ID3D11InputLayout> m_PrimitiveLayout;
-    std::unique_ptr<CommonStates> m_PrimitiveStates;
 };
